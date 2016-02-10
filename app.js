dc5ea3fce1d94c17c82f489305bec7ac56270ea7
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-02-10 10:49:30 +0200

    added singleton pattern

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
- 
+(function(){
+  "use strict";
+
+
+  var Moosipurk = function(){
+
+    // SINGLETON PATTERN (4 rida)
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+    Moosipurk.instance = this; // this viitab moosipurgile
+
+
+    console.log('moosipurgi sees');
+  };
+
+
+  window.onload = function(){
+    var app = new Moosipurk();
+  };
+
+})();
