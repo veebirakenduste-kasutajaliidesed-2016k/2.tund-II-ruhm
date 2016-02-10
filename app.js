3c8860d4b3fcf8e410bb6c84038e4c7828b720a0
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-02-10 11:05:16 +0200

    added functions

--- app.js
+++ app.js
@@ -10,8 +10,28 @@
     }
     Moosipurk.instance = this; // this viitab moosipurgile
 
+    console.log(this);
+    //console.log('moosipurgi sees');
 
-    console.log('moosipurgi sees');
+    //panen rakenduse tööle
+    this.init();
+  };
+
+  //kõik moosipurgi funktsioonid tulevad siia sisse
+  Moosipurk.prototype = {
+    init: function(){
+      console.log('rakendus käivitus');
+      // Siia tuleb esialgne loogika
+      // hakka kuulama hiireklõpse
+      this.bindMouseEvents();
+    },
+    bindMouseEvents: function(){
+      document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+    },
+    addNewClick: function(event){
+      console.log(event);
+
+    }
   };
 
 
